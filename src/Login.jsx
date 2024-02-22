d0dbe3f27034af63d3295daa7c0c82240a1b14b1
Krishna <krp345@gmail.com>
2024-02-22 21:41:35 +0530

    first commit

---
+++ src/Login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Login(){
+    return(
+        <div className="formContainer">
+        <div className="formWrapper">
+          {/* <span className="logo">Login</span> */}
+          <span className="title">Login</span>
+            <form>
+                <input type="email" placeholder="email"/>
+                <input type="password" placeholder="password"/>      
+                <button>Sign in</button>   
+                {/* {err && <span>Something went wrong </span>} */}
+            </form>
+            {/* <p>Don't have an Account? <Link to="/register">Register</Link></p> */}
+        </div>
+    </div>
+    );
+}
+
+export default Login;
